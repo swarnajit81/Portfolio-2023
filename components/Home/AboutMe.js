4153096476ba01f38b59dce0fddd0bbe46790bf6
jordann81 <jordan.b@mediaslide.com>
2025-02-04 00:37:06 +0530

    fix:overflowing text

--- components/Home/AboutMe.js
+++ components/Home/AboutMe.js
@@ -19,17 +19,8 @@ const AboutMe = () => {
         </div>
         <div className="max-w-[30rem] ml-auto mt-auto flex items-end h-[60svh]">
           <p>
-            I&apos;ve always been an artist, an admirer of mysticism and
-            abstract arts. And creating immersive web experiences that leaves
-            user itself in awe, is an art which has been justified. I thrive for
-            that awe considering me being the artist and creator of such web
-            canvas. As Nietzsche said ,{" "}
-            <q>
-              art is the highest form of self-expression and a way to affirm
-              life
-            </q>
-            <br />
-            <br />I am a fan of new ideas and brainstorming it in an engaging
+           
+            I am a fan of new ideas and brainstorming it in an engaging
             way. After 4 years of voluteering on different techstacks for IT
             firms, shaping and sharpening my problem solving and artistic
             ability, I have decided to help talented enthusiasts by providing a
