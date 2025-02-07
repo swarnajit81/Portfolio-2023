0b59e358cc4711f5180a7c146f00db934139b2b6
jordann81 <jordan.b@mediaslide.com>
2025-02-07 19:13:51 +0530

    update:keywords

--- components/Common/meta.js
+++ components/Common/meta.js
@@ -4,27 +4,31 @@ import React from "react";
 
 const Meta = ({
   title = "Swarnajit | Creative Web Developer",
-  keywords = `
-web development, interactive design, creative coding, immersive web experiences, UI/UX design, frontend development, animations, performance optimization, digital artistry, abstract art, problem-solving, web animations, user experience, creative solutions, portfolio showcase, web canvas, independent developer, web aesthetics, artistic web design, engaging websites`,
-  description = "Passionate artist and web creator crafting immersive digital experiences with a focus on interaction, animations, and performance. With 4+ years of experience across various tech stacks, I bring refined, creative solutions to help enthusiasts and businesses push boundaries. Let’s collaborate to make something extraordinary.",
-  imgContent = "https://swarnajit.space/images/logo.png",
+  keywords = "Swarnajit, Space, swarnajit, sj immersive, swarnajit space, web development, interactive design, creative coding, immersive web experiences, UI/UX design, frontend development, animations, performance optimization, digital artistry, abstract art, problem-solving, web animations, user experience, creative solutions, portfolio showcase, web canvas, independent developer, web aesthetics, artistic web design, engaging websites",
+  imgContent = "/images/logo.png",
 }) => {
   const router = useRouter();
+  const description =
+    "Passionate artist and web creator crafting immersive digital experiences with a focus on interaction, animations, and performance. With 4+ years of experience across various tech stacks, I bring refined, creative solutions to help enthusiasts and businesses push boundaries. Let’s collaborate to make something extraordinary.";
   return (
     <Head>
       <title>{title}</title>
       <meta name="AUTHOR" content={"swarnajit.space"} />
       <meta name="copyright" content={"swarnajit.space"} />
 
-      <meta name="description" content={description} />
+      <meta
+        name="description"
+        content={
+          "Passionate artist and web creator crafting immersive digital experiences with a focus on interaction, animations, and performance. With 4+ years of experience across various tech stacks, I bring refined, creative solutions to help enthusiasts and businesses push boundaries. Let’s collaborate to make something extraordinary."
+        }
+      />
       <meta name="keywords" content={keywords} />
 
       {/* <!-- for Facebook -->           */}
       <meta property="og:title" content={title} />
       <meta property="og:type" content="article" />
       <meta property="og:image" content={imgContent} />
-      <meta property="og:image:width" content="500" />
-      <meta property="og:image:height" content="500" />
+
       <meta
         property="og:url"
         content={"https://swarnajit.space" + router.asPath}
